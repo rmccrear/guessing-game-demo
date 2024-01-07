3b0a9285257af0636d3d134cf0bb6af693f6d273
Robert McCreary <113715288+rmccrear@users.noreply.github.com>
2024-01-07 02:09:06 +0000

    create guessing game

---
+++ script.js
@@ -0,0 +1,25 @@
+// Let's guess what this code does!
+
+// Write down what you think each line does.
+
+// It's OK if you're not sure, that's why it's a guess!
+
+alert("Ready, player 1?");
+
+confirm("Let's play a guessing game.")
+
+let g = prompt("Guess a number...");
+
+let guess = parseInt(g);
+
+if(guess === 7) {
+  alert("You got it!");
+} else {
+  alert("Nope!")
+}
+
+alert("Thanks for playing!");
+
+// To run this code, copy it and past it into the Console in the Dev Tools.
+
+// Adapted from Head First Programming by Paul Barry
